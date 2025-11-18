088d449914a3a516b7b90651df7be9c6b43393c4
Punit Yadav <punit965@gmail.com>
2025-11-19 03:32:34 +0530

    feat: logo added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,14 @@
 import "./../styles/header.css";
 import DarkModeToggle from "./dark-mode-toggle";
+import SvgLogo from "./logo";
 
 export default function Header() {
   return (
     <header>
-      <h1 className="logo">Create CV</h1>
+      <h1 className="logo">
+        <SvgLogo />
+        Create CV
+      </h1>
       <DarkModeToggle />
     </header>
   );
